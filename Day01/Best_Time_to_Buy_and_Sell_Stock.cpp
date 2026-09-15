329f293824e640c4287b3803cb6edbcbb97b9fd8
nandanabain <nandanabain@gmail.com>
2026-09-15 23:09:16 +0530

    Add Best Time to Buy and Sell Stock solution

---
+++ Day01/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,24 @@
+// LeetCode #121 - Best Time to Buy and Sell Stock
+// Approach: Track minimum price
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int i = 1; i < prices.size(); i++)
+        {
+            int profit = prices[i] - minPrice;
+
+            maxProfit = max(maxProfit, profit);
+
+            minPrice = min(minPrice, prices[i]);
+        }
+
+        return maxProfit;
+    }
+};
