d6cd21882709feb1e4a6b58985dc865c5cadcba1
nandanabain <nandanabain@gmail.com>
2026-09-18 20:41:25 +0530

    Add Day 4 Intersection of Two Arrays solution

---
+++ Day04/Intersection_of_Two_Arrays.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+    {
+        vector<int> ans;
+
+        for(int i = 0; i < nums1.size(); i++)
+        {
+            for(int j = 0; j < nums2.size(); j++)
+            {
+                if(nums1[i] == nums2[j])
+                {
+                    if(find(ans.begin(), ans.end(), nums1[i]) == ans.end())
+                    {
+                        ans.push_back(nums1[i]);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
